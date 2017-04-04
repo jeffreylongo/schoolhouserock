e3ea820941eda5a170fc5ff3f119e49c3f118fc2
Jeff Longo <jeffreylongo@yahoo.com>
2017-04-04 16:24:09 -0400

    had to rebuild...was making .net core application on accident.

---
+++ schoolhouserock1/schoolhouserock1/Courses.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace schoolhouserock
+{
+    class Courses
+    {
+        private object reader;
+
+        public string Title { get; set; }
+        public string Instructor { get; set; }
+        public int CourseNumber { get; set; }
+        public int DeptCode { get; set; }
+
+        public Courses()
+        {
+
+        }
+
+        public Courses(SqlDataReader reader)
+        {
+            Title = reader["Title"].ToString();
+            Instructor = reader["Instructor"].ToString();
+            CourseNumber = (int)reader["CourceNumber"];
+            DeptCode = (int)reader["DeptCode"];
+
+        }
+
+        public Courses(object reader)
+        {
+            this.reader = reader;
+        }
+    }
+}
---
+++ schoolhouserock1/schoolhouserock1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace schoolhouserock
+{
+    class Program
+    {
+        //making method to get courses...I think
+        static public List<Courses> GetAllCourses(SqlConnection connection)
+        {
+            var courses = new List<Courses>();
+            //establish the Query
+            var sqlCommand = new SqlCommand(@"SELECT *
+                                                FROM Course", connection);
+            //open th connection
+            connection.Open();
+            //run Query
+            var reader = sqlCommand.ExecuteReader();
+            //read results
+
+            while (reader.Read())
+            {
+                var course = new Courses(reader);
+                courses.Add(course);
+            }
+            //close connection
+            connection.Close();
+            return courses;
+        }
+        static void Greeting()
+        {
+            Console.WriteLine("Welcome to my madeup College Hub. Lets see if anything happens.");
+        }
+        static void Main(string[] args)
+        {
+            //user greeting
+            Greeting();
+            Console.ReadLine();
+            //connect to database
+            const string connectionString =
+                @"Server=localhost\SQLEXPRESS;Database=SchoolHouseRock;Trusted_Connection=True;";
+            //open with using something
+            var courses = new List<Courses>();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                courses = GetAllCourses(connection);
+                foreach (var course in courses)
+                {
+                    Console.WriteLine(course.Title);
+                }
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
